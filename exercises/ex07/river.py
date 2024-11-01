d7f4b8badb9839233013b883cee412614fb92e69
Sophie Jiang <84470289+sjiang13@users.noreply.github.com>
2024-11-01 02:07:25 -0400

    Update import path in river.py

--- exercises/ex07/river.py
+++ exercises/ex07/river.py
@@ -1,7 +1,7 @@
 """File to define River class."""
 
-from ex07.fish import Fish
-from ex07.bear import Bear
+from exercises.ex07.fish import Fish
+from exercises.ex07.bear import Bear
 
 class River:
     
